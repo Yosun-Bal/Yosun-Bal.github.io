b37d18c9b5ed619fef4336276e9ba6c1599c87a6
Yosun-Bal <160943663+Yosun-Bal@users.noreply.github.com>
2024-02-22 22:11:14 +0000

    Add dynamic reordering of language dropdown

--- languageSwitcher.js
+++ languageSwitcher.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   var selector = document.getElementById('languageSwitcher');
   if (selector) {
+    // Set the current language as selected based on the page's language.
+    const currentLang = document.body.className.match(/lang-(\w+)/);
+    if (currentLang && currentLang[1]) {
+      selector.value = `index_${currentLang[1]}.html`; // Dependency on the file naming scheme
+    }
+
+    // Listen for changes to redirect to the selected language version.
     selector.addEventListener('change', function() {
       window.location.href = this.value;
     });
